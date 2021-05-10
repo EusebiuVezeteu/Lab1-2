ab9872d0e1a7341e2decdaf5562e563e86c576ec
EusebiuVezeteu <83941256+EusebiuVezeteu@users.noreply.github.com>
2021-05-10 09:19:28 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"n=";cin>>n;
+    if((a>>2)==0)
+    {
+        cout<<"Nr este de forma 2k"
+    }
+    else
+    {
+        cout<<"Nr nu este de forma 2k"
+    }
+    return 0;
+}
